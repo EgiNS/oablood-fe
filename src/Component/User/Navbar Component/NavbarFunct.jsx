99679f15130404de46c5f1995102f1c517f202da
iqi <rifqiaulia987@gmail.com>
2021-09-08 19:44:56 +0700

    fix navbar user

--- src/Component/User/Navbar Component/NavbarFunct.jsx
+++ src/Component/User/Navbar Component/NavbarFunct.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
-import logo from './Assets/Logo.svg';
-import ayoDonor from './Assets/Donor.svg';
-import poin from './Assets/Poin.svg';
-import beranda from './Assets/Beranda.svg';
-import reqDarah from './Assets/Request.svg';
-import reservasi from './Assets/Reservasi.svg';
-import reward from './Assets/Reward.svg';
-import user from './Assets/User.svg';
-import ads from './Assets/Ads.svg';
-import logout from './Assets/Logout.svg';
-import './Assets/Style.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Collapse, NavbarToggler, Nav, Navbar } from 'reactstrap';
+import React, { useState } from "react";
+import logo from "./Assets/Logo.svg";
+import ayoDonor from "./Assets/Donor.svg";
+import poin from "./Assets/Poin.svg";
+import beranda from "./Assets/Beranda.svg";
+import reqDarah from "./Assets/Request.svg";
+import reservasi from "./Assets/Reservasi.svg";
+import reward from "./Assets/Reward.svg";
+import user from "./Assets/User.svg";
+import ads from "./Assets/Ads.svg";
+import logout from "./Assets/Logout.svg";
+import home from "./Assets/home.svg";
+import "./Assets/Style.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Link,
+} from "react-router-dom";
+import { Collapse, NavbarToggler, Nav, Navbar } from "reactstrap";
 
 export default function NavbarComp(props) {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,15 +40,19 @@ export default function NavbarComp(props) {
                   <Link to="/profile" className="nav-link">
                     <div className="user-profile">
                       <div id="user-pic">
-                        <img src={user} alt="" style={{width:"2vw"}}/>
+                        <img src={user} alt="" style={{ width: "2vw" }} />
                       </div>
                       <div className="user-namepoin">
-                        <p id="user-name">{localStorage.getItem('name')}</p>
+                        <p id="user-name">{localStorage.getItem("name")}</p>
                         <div className="user-poin">
                           <img src={poin} alt="" />
                           <p id="user-count">
-                            {' '}
-                            <span id="point"> {localStorage.getItem('point')} </span> Poin
+                            {" "}
+                            <span id="point">
+                              {" "}
+                              {localStorage.getItem("point")}{" "}
+                            </span>{" "}
+                            Poin
                           </p>
                         </div>
                       </div>
@@ -50,63 +60,70 @@ export default function NavbarComp(props) {
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <div className="nav-beranda nav-content" id="0">
-                    <img src={beranda} alt="" />
-                    <Link to="/beranda" className="nav-link" style={{ color: 'white' }}>
-                      {' '}
-                      Beranda{' '}
-                    </Link>
-                  </div>
+                  {/* <div className="nav-content" id="0"> */}
+                  <NavLink
+                    to="/beranda"
+                    className="nav-link nav-content"
+                    // style={{ color: "white" }}
+                    activeClassName="nav-active"
+                  >
+                    <img src={home} alt="" /> Beranda{" "}
+                  </NavLink>
                 </li>
-                <li className="nav-item" style={{borderRadius:"15px"}}>
-                  <div className="nav-content">
-                    <img src={ayoDonor} alt="" />
-                    <Link to="/donor" className="nav-link" style={{ color: '#8A94A6;' }}>
-                      {' '}
-                      Donor{' '}
-                    </Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <div className="nav-content">
-                    <img src={reqDarah} alt="" />
-                    <Link to="/request" className="nav-link" style={{ color: '#8A94A6;' }}>
-                      {' '}
-                      Request{' '}
-                    </Link>
-                  </div>
+                <li className="nav-item" style={{ borderRadius: "15px" }}>
+                  <NavLink
+                    to="/donor"
+                    className="nav-link nav-content"
+                    activeClassName="nav-active"
+                  >
+                    <img src={ayoDonor} alt="" /> Donor{" "}
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <div className="nav-content">
-                    <img src={reservasi} alt="" />
-                    <Link to="/reservasi" className="nav-link" style={{ color: '#8A94A6;' }}>
-                      {' '}
-                      Reservasi{' '}
-                    </Link>
-                  </div>
+                  <NavLink
+                    to="/request"
+                    className="nav-link nav-content"
+                    activeClassName="nav-active"
+                  >
+                    <img src={reqDarah} alt="" /> Request{" "}
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <div className="nav-content">
-                    <img src={reward} alt="" />
-                    <Link to="/reward" className="nav-link" style={{ color: '#8A94A6;' }}>
-                      {' '}
-                      Reward{' '}
-                    </Link>
-                  </div>
+                  <NavLink
+                    to="/reservasi"
+                    className="nav-link nav-content"
+                    activeClassName="nav-active"
+                  >
+                    <img src={reservasi} alt="" /> Reservasi{" "}
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    to="/reward"
+                    className="nav-link nav-content"
+                    activeClassName="nav-active"
+                  >
+                    <img src={reward} alt="" /> Reward{" "}
+                  </NavLink>
                 </li>
                 <div className="nav-ads">
-                  <Link to="/upgrade">
-                    {' '}
-                    <img src={ads} alt="" />{' '}
-                  </Link>
+                  <NavLink to="/upgrade">
+                    {" "}
+                    <img src={ads} alt="" />{" "}
+                  </NavLink>
                 </div>
                 <li className="nav-item">
                   <div className="nav-content">
                     <img src={logout} alt="" />
-                    <Link to="/" className="nav-link" style={{ color: '#8A94A6;' }} onClick={() => localStorage.clear()}>
-                      {' '}
-                      Logout{' '}
-                    </Link>
+                    <NavLink
+                      to="/"
+                      className="nav-link"
+                      style={{ color: "#8A94A6;" }}
+                      onClick={() => localStorage.clear()}
+                    >
+                      {" "}
+                      Logout{" "}
+                    </NavLink>
                   </div>
                 </li>
               </ul>
